9a0937dce9dc2226f4c3f497a4e8684d2c39f01b
fozb <matze@clubdesign.de>
2014-09-14 13:59:04 +0200

    initial module skeleton commit

---
+++ Module.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *
+ */
+
+// Change the Namespace to your needs
+namespace Application;
+/**
+ * Class Module
+ * @package Application
+ */
+class Module
+{
+    public function onBootstrap()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * getAutoloaderConfig
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * getServiceConfig
+     *
+     * @return array
+     */
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+            ]
+        ];
+    }
+}
